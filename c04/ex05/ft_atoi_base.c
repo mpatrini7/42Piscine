d9f398d9bc584d9f0a24c55ffc2fc7cb834827fd
Michael Patrini <mpatrini@e3r1p8.42roma.it>
2021-12-07 20:31:29 +0100

    atoi_base

--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -9,92 +9,93 @@
 /*   Updated: 2021/12/03 23:48:58 by mpatrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+#include <stdio.h>
 
 int	ft_is_space(char a)
 {
-	if (a == 32 || a == '\t' || a == '\v' || a == '\f' || a == '\r')
+	if (a == 32 || a == '\t' || a == '\v' || a == '\f'
+		|| a == '\r' || a == '\n')
 		return (1);
 	return (0);
 }
 
-int	ft_check_same_char(char *base)
+int	ft_strlen(char *str)
 {
 	int	i;
-	int	in;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_valid_base(char *str)
+{
+	char	*current;
+	int		i;
+	int		o;
+
+	current = str;
+	i = 0;
+	if (str == 0 || ft_strlen(str) <= 1)
+		return (0);
+	while (*current)
+	{
+		if (ft_is_space(*current) || *current == '+' || *current == '-')
+			return (0);
+		current++;
+	}
+	while (i < current - str)
+	{
+		o = i + 1;
+		while (o < current - str)
+			if (str[i] == str[o++])
+				return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_base(char *base, char a)
+{
+	int	i;
 
 	i = 0;
 	while (base[i])
 	{
-		in = i + 1;
-		while (base[in])
-		{
-			if (base[i] == base[in])
-				return (1);
-			in++;
-		}
+		if (base[i] == a)
+			return (i);
 		i++;
 	}
-	return (0);
-}
-
-int	ft_atoi2(char *str)
-{
-	int	i;
-	int	sign;
-	int	save;
-
-	sign = 1;
-	i = 0;
-	save = 0;
-	while (str[i])
-	{
-		if (str[i] >= 48 && str[i] <= 57)
-			save = (save * 10) + (str[i] - 48);
-		else
-			return (0);
-		i++;
-	}
-	return (save * sign);
-}
-
-int	ft_atoi(char *str, int base)
-{
-	int	i;
-	int	sign;
-	int	save;
-
-	sign = 1;
-	i = 0;
-	save = 0;
-	while (ft_is_space(str[i]) == 1)
-		i++;
-	while (str[i] == 45 || str[i] == 43)
-	{
-		if (str[i] == 45)
-			sign *= -1;
-		i++;
-	}
-	while (str[i])
-	{
-		if (str[i] >= 48 && str[i] <= 57)
-			save = (save * base) + (str[i] - 48);
-		else
-			break ;
-		i++;
-	}
-	return (save * sign);
+	return (-1);
 }
 
 int	ft_atoi_base(char *str, char *base)
-{	
-	int	at_base;
+{
+	int	mult;
+	int	save;
+	int	sign;
+	int	baseb;
 
-	if (*base == '\0' || base[1] == '\0')
+	save = 0;
+	sign = 1;
+	if (ft_valid_base(base) == 0)
 		return (0);
-	if (ft_check_same_char(base) == 1)
-		return (0);
-	at_base = ft_atoi2(base);
-	if (at_base == 0)
-		return (0);
-	return (ft_atoi(str, at_base));
+	mult = ft_strlen(base);
+	while (ft_is_space(*str) == 1)
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (ft_base(base, *str) != -1)
+	{
+		baseb = ft_base(base, *str);
+		save = (save * mult) + baseb;
+		str++;
+	}
+	return (save * sign);
 }
