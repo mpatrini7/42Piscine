2c13cd76447cb85c47f3120101edb52a3f6dc465
Michael Patrini <mpatrini@e3r1p8.42roma.it>
2021-12-16 20:27:02 +0100

    c08

--- c08/ex04/ft_strs_to_tab.c
+++ c08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,52 @@
 /*   By: mpatrini <mpatrini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 22:41:00 by mpatrini          #+#    #+#             */
-/*   Updated: 2021/12/15 22:41:01 by mpatrini         ###   ########.fr       */
+/*   Updated: 2021/12/16 18:41:44 by mpatrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+char	*ft_strcpy(char *str, int size)
+{
+	char	*copy;
+	int		i;
+
+	copy = 0;
+	copy = malloc(sizeof(char) * (size + 1));
+	i = 0;
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i += 1;
+	}
+	copy[i] = 0;
+	return (copy);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*stock_str;
+	int			i;
+	int			len;
+
+	stock_str = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (stock_str == 0)
+		return (0);
+	i = 0;
+	while (i < ac)
+	{
+		len = 0;
+		while (av[i][len])
+			len++;
+		stock_str[i].size = len;
+		stock_str[i].str = av[i];
+		stock_str[i].copy = ft_strcpy(av[i], stock_str[i].size);
+		i += 1;
+	}
+	stock_str[i].size = 0;
+	stock_str[i].str = 0;
+	stock_str[i].copy = 0;
+	return (stock_str);
+}
--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -6,7 +6,67 @@
 /*   By: mpatrini <mpatrini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 22:41:03 by mpatrini          #+#    #+#             */
-/*   Updated: 2021/12/15 22:41:04 by mpatrini         ###   ########.fr       */
+/*   Updated: 2021/12/16 18:41:42 by mpatrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av);
+
+int	str_l(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+void	put_char(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		put_char('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		put_char(nb + '0');
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		write(1, par[i].str, str_l(par[i].str));
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		write(1, par[i].copy, str_l(par[i].str));
+		write(1, "\n", 1);
+		i++;
+	}
+}
